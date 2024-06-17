80a6802a4692f46233182ffe8db9411bf03c7f01
Kailin Tang <kailin.tang@limblecmms.com>
2024-06-16 20:57:34 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ module.exports.loop = function () {
         Memory.tickCount = 0
     }
     else {
-        if(Memory.tickCount == 1200) {
+        if(Memory.tickCount % 300 == 0) {
             console.log("|||||| tick", Memory.tickCount)
             console.log("|||||| level", Game.rooms.sim.controller.level)
             console.log("|||||| progress", Game.rooms.sim.controller.progress)
